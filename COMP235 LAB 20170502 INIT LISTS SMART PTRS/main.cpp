338e38fc572844fe8c8b39de7ebff6088e57748a
aberry5621 <aberry5621@gmail.com>
2017-05-03 09:14:25 -0700

    Initial Commit

---
+++ COMP235 LAB 20170502 INIT LISTS SMART PTRS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP235 LAB 20170502 INIT LISTS SMART PTRS
+//
+//  Created by ax on 5/3/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
